e247f262a50c721c72e26d582d42894423dd775b
rsanlazaro <rsanlazaro@hotmail.com>
2025-07-06 21:26:04 -0600

    db test

--- views/babySites/pro_gestor/users.php
+++ views/babySites/pro_gestor/users.php
@@ -503,7 +503,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 <!-- Pagination -->
 <script>
     let options = {
-        numberPerPage: 30, //Cantidad de datos por pagina
+        numberPerPage: 10000000000000, //Cantidad de datos por pagina
         goBar: true, //Barra donde puedes digitar el numero de la pagina al que quiere ir
         pageCounter: true, //Contador de paginas, en cual estas, de cuantas paginas
     };
